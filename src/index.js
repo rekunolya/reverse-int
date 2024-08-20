2bb5434343d49eb7a3492eefeb3a79f05ea68639
rekunolya <rekun.olya@gmail.com>
2024-08-20 22:29:59 +0300

    feat: add a solution to task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let string = String(n);
+
+    let arrTemp = string.split('-');
+    string = arrTemp.join('');
+    arrTemp = string.split('');
+    let res = arrTemp.reverse().join('');
+    return res;
 }
